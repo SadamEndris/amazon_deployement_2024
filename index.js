ddcef3db4c2b982cc37c8b4c90acfeebe0558087
Sa-Dev <sadamendris48@gmail.com>
2024-07-24 18:52:25 +0300

    amazon backend deployment

---
+++ index.js
@@ -0,0 +1,39 @@
+// require express
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    // console.log(paymentIntent);
+    // res.status(200).json(paymentIntent);
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(400).send({
+      message: "Invalid amount, Total must be greater than 0",
+    });
+  }
+});
+
+// listen through express
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Amazon Server is running on port: http://localhost:${PORT}`);
+});
